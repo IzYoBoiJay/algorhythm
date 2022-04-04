35527808d6ff8255ab470c2ef645bd1954e04635
IzYoBoiJay <JTCalahatian@gmail.com>
2022-04-04 01:51:32 -0400

    New Dependencies, Styled, Useless Removed, Router

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,9 +1,11 @@
 
 import React from 'react';
+import Navbar from './Navbar';
 
 function Login() {
     return (
         <div className="App">
+            <Navbar/>
             <header className="App-header">
                 <a className="btn-spotify" href="/auth/login" >
                     Login with Spotify 
