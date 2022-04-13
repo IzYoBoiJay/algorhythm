7efa85a91fe81cb868270e506eeae2b557903694
sjhnandez <sj.hnandez@gmail.com>
2022-04-13 07:55:41 -0400

    Formatting, add logout, auth context

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -1,41 +1,51 @@
-import React, { useState, useEffect } from 'react';
-import {UserCointainer, ProfilePicture, Username, UsernameContainer} from './User'
+import React, { useState, useEffect, useContext } from "react";
+import {
+  UserCointainer,
+  ProfilePicture,
+  Username,
+  UsernameContainer,
+} from "./User";
+
+import UserContext from "../../contexts/UserContext";
 
 const GetUserData = async (token) => {
-    const settings = {
-    headers: {'Authorization': 'Bearer ' + token}
-    }
-    const response = await fetch('https://api.spotify.com/v1/me', settings);
-    const data = await response.json()
-    console.log(data)
-    return data;
-}
+  const settings = {
+    headers: { Authorization: "Bearer " + token },
+  };
+  const response = await fetch("https://api.spotify.com/v1/me", settings);
+  const data = await response.json();
+  console.log(data);
+  return data;
+};
 
 function GetUserInfo(props) {
-    const [profile, setProfile] = useState(null);
-    useEffect(() => {
-        console.log(props.token);
-        async function fetchData() {
-        if(props.token != null) {
-                setProfile(await GetUserData(props.token));
-            }
-        }
-        fetchData();
-    }, [props.token]);
+  const token = useContext(UserContext);
 
-    return (
-        <>
-        {profile && (
-          <UserCointainer>
-            {profile.images.length && profile.images[0].url && (
-              <ProfilePicture image={profile.images[0].url} alt="Avatar"/>
-            )}
-            <UsernameContainer width={profile.display_name.length*25}><Username>{profile.display_name}</Username></UsernameContainer>
-            {/* <p>{profile.followers.total} Followers</p> */}
-          </UserCointainer>
-        )}
-      </>
-    );
-  }
-  
+  const [profile, setProfile] = useState(null);
+  useEffect(() => {
+    async function fetchData() {
+      if (token != null) {
+        setProfile(await GetUserData(token));
+      }
+    }
+    fetchData();
+  }, [token]);
+
+  return (
+    <>
+      {profile && (
+        <UserCointainer>
+          {profile.images.length && profile.images[0].url && (
+            <ProfilePicture image={profile.images[0].url} alt="Avatar" />
+          )}
+          <UsernameContainer width={profile.display_name.length * 25}>
+            <Username>{profile.display_name}</Username>
+          </UsernameContainer>
+          {/* <p>{profile.followers.total} Followers</p> */}
+        </UserCointainer>
+      )}
+    </>
+  );
+}
+
 export default GetUserInfo;
--- src/pages/ExplorePage.js
+++ src/pages/ExplorePage.js
@@ -1,106 +1,113 @@
-import React from 'react'
-import styled from 'styled-components';
-import Post from '../components/Post';
-import WebPlayback from '../components/WebPlayback/WebPlayback';
-import Masonry, {ResponsiveMasonry} from 'react-responsive-masonry';
+import React from "react";
+import styled from "styled-components";
+import Post from "../components/Post";
+import WebPlayback from "../components/WebPlayback/WebPlayback";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
 /* Fake data */
 
-import songPicture from '../images/cheems.jpg'
-import pfp from '../images/bruh.jpg'
+import songPicture from "../images/cheems.jpg";
+import pfp from "../images/bruh.jpg";
 
 const posts = [
   {
-    id: 1, 
-    username: "Mike Wazowski", 
-    profilepic: pfp, 
-    postText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tincidunt ornare massa eget egestas purus. Donec adipiscing tristique risus nec feugiat. Aliquam faucibus purus in massa tempor. Faucibus et molestie ac feugiat. Non diam phasellus vestibulum lorem sed risus ultricies tristique nulla.",
-    songInfo:
-    {
+    id: 1,
+    username: "Mike Wazowski",
+    profilepic: pfp,
+    postText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tincidunt ornare massa eget egestas purus. Donec adipiscing tristique risus nec feugiat. Aliquam faucibus purus in massa tempor. Faucibus et molestie ac feugiat. Non diam phasellus vestibulum lorem sed risus ultricies tristique nulla.",
+    songInfo: {
       picture: songPicture,
       title: "Who the hecc is Bam the Dog?",
       album: "Sample Text Album",
-      artist: "Cheems"
+      artist: "Cheems",
     },
     datetime: "1 day ago",
     hashtag: ["sad", "very-sad"],
   },
   {
-    id: 2, 
-    username: "Tan", 
-    profilepic: "https://i.pinimg.com/736x/c7/6f/34/c76f34d8e157415d37978883f1b596bc.jpg", 
-    postText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tincidunt ornare massa eget egestas purus.",
-    songInfo:
-    {
-      picture: "http://pm1.narvii.com/7308/c94c95c8e63694384a383a8e480e69f30ee7176ar1-640-640v2_uhq.jpg",
+    id: 2,
+    username: "Tan",
+    profilepic:
+      "https://i.pinimg.com/736x/c7/6f/34/c76f34d8e157415d37978883f1b596bc.jpg",
+    postText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tincidunt ornare massa eget egestas purus.",
+    songInfo: {
+      picture:
+        "http://pm1.narvii.com/7308/c94c95c8e63694384a383a8e480e69f30ee7176ar1-640-640v2_uhq.jpg",
       title: "Tan",
       album: "Tan tan tan",
-      artist: "Tannie"
+      artist: "Tannie",
     },
     datetime: "2 days ago",
-    hashtag: ["tannie", "tannieee"]
+    hashtag: ["tannie", "tannieee"],
   },
   {
-    id: 3, 
-    username: "Bam", 
-    profilepic: "https://worldhours24.com/wp-content/uploads/2021/11/Screenshot_64.png", 
-    postText: "Non diam phasellus vestibulum lorem sed risus ultricies tristique nulla.",
-    songInfo:
-    {
-      picture: "https://st1.bollywoodlife.com/wp-content/uploads/2022/01/FotoJet-2022-01-01T230946.153.jpg",
+    id: 3,
+    username: "Bam",
+    profilepic:
+      "https://worldhours24.com/wp-content/uploads/2021/11/Screenshot_64.png",
+    postText:
+      "Non diam phasellus vestibulum lorem sed risus ultricies tristique nulla.",
+    songInfo: {
+      picture:
+        "https://st1.bollywoodlife.com/wp-content/uploads/2022/01/FotoJet-2022-01-01T230946.153.jpg",
       title: "Bam",
       album: "Bam bam bam",
-      artist: "Bam Jeon"
+      artist: "Bam Jeon",
     },
     datetime: "3 days ago",
-    hashtag:["bambam", "bam"]
+    hashtag: ["bambam", "bam"],
   },
   {
-    id: 4, 
-    username: "Coke", 
-    profilepic: "https://pbs.twimg.com/profile_images/1357949524858306563/orJz7v6J_400x400.jpg", 
-    postText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tincidunt ornare massa eget egestas purus. Donec adipiscing tristique risus nec feugiat. Aliquam faucibus purus in massa tempor. Faucibus et molestie ac feugiat. Non diam phasellus vestibulum lorem sed risus ultricies tristique nulla.",
-    songInfo:
-    {
-      picture: "https://pbs.twimg.com/media/FPg-eNgUcAMz2YP?format=jpg&name=large",
+    id: 4,
+    username: "Coke",
+    profilepic:
+      "https://pbs.twimg.com/profile_images/1357949524858306563/orJz7v6J_400x400.jpg",
+    postText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tincidunt ornare massa eget egestas purus. Donec adipiscing tristique risus nec feugiat. Aliquam faucibus purus in massa tempor. Faucibus et molestie ac feugiat. Non diam phasellus vestibulum lorem sed risus ultricies tristique nulla.",
+    songInfo: {
+      picture:
+        "https://pbs.twimg.com/media/FPg-eNgUcAMz2YP?format=jpg&name=large",
       title: "i am Sneazing",
       album: "Achoo",
-      artist: "Coke Zero"
+      artist: "Coke Zero",
     },
     datetime: "1 week ago",
-    hashtag:["cokezero", "coke"]
+    hashtag: ["cokezero", "coke"],
   },
   {
-    id: 5, 
-    username: "fatfatpankocat", 
-    profilepic: "https://pbs.twimg.com/media/FQLxfQ0UYAEmk15?format=jpg&name=large", 
-    postText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tincidunt ornare massa eget egestas purus.",
-    songInfo:
-    {
-      picture: "https://pbs.twimg.com/media/FQLtdwFXIAQ7xUp?format=jpg&name=large",
+    id: 5,
+    username: "fatfatpankocat",
+    profilepic:
+      "https://pbs.twimg.com/media/FQLxfQ0UYAEmk15?format=jpg&name=large",
+    postText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tincidunt ornare massa eget egestas purus.",
+    songInfo: {
+      picture:
+        "https://pbs.twimg.com/media/FQLtdwFXIAQ7xUp?format=jpg&name=large",
       title: "Meow",
       album: "Pankooooooooo",
-      artist: "Panko Cat"
+      artist: "Panko Cat",
     },
     datetime: "1 week ago",
-    hashtag:["meow"]
+    hashtag: ["meow"],
   },
   {
-    id: 6, 
-    username: "Sample", 
-    profilepic: "", 
+    id: 6,
+    username: "Sample",
+    profilepic: "",
     postText: "Sample",
-    songInfo:
-    {
+    songInfo: {
       picture: "",
       title: "Sample Title",
       album: "Sample Text Album",
-      artist: "Sample"
+      artist: "Sample",
     },
     datetime: "1 week ago",
-    hashtag: ["sample", "sample2"]
-  }
-]
+    hashtag: ["sample", "sample2"],
+  },
+];
 
 export const ExploreContainer = styled.section`
   padding: 50px;
@@ -109,14 +116,16 @@ export const ExploreContainer = styled.section`
 const ExplorePage = (props) => {
   return (
     <ExploreContainer>
-      <ResponsiveMasonry columnsCountBreakPoints={{350: 1, 750: 2, 900: 4}}>
+      <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 4 }}>
         <Masonry gutter="20px">
-          <WebPlayback token = {props.token}/>
-          {posts.map(post => <Post post={post}/>)}
-          </Masonry>
+          <WebPlayback />
+          {posts.map((post) => (
+            <Post post={post} />
+          ))}
+        </Masonry>
       </ResponsiveMasonry>
     </ExploreContainer>
-  )
-}
+  );
+};
 
 export default ExplorePage;
